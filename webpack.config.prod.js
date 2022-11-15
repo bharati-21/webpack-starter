efcea80ba9ad26d377143afc518be58a35c714ff
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-15 21:07:10 +0530

    feat: clean webpack plugin, prod, dev config (#1)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,9 +2,6 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.config.common");
 
 module.exports = merge(commonConfig, {
-	devtool: "inline-source-map", // not to be done in production mode
-	devServer: {
-		static: "./dist",
-	},
-	mode: "development",
+	devtool: "source-map",
+	mode: "production",
 });
